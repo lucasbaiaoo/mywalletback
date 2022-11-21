7c2b335f53b0667ba6c0c1c1b865172fc53c3d40
luskamusca <luskamuskammouse@gmail.com>
2022-11-21 00:59:40 -0300

    feat: create POST signin

--- src/app.js
+++ src/app.js
@@ -4,16 +4,17 @@ import { MongoClient, ObjectId } from "mongodb";
 import dotenv from "dotenv";
 import joi from "joi";
 import bcrypt from "bcrypt";
+import { v4 as uuid } from "uuid";
 
 dotenv.config();
 
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
 
-try{
-    await mongoClient.connect();
+try {
+  await mongoClient.connect();
 } catch (error) {
-    console.log(error);
+  console.log(error);
 }
 
 db = mongoClient.db("myWallet");
@@ -23,16 +24,36 @@ server.use(cors());
 server.use(express.json());
 
 server.post("/sign-up", async (req, res) => {
-    const user = req.body;
-    const hashPassword = bcrypt.hashSync(user.password, 10);
-  
-    try {
-      await db.collection("users").insertOne({ ...user, password: hashPassword });
-      res.sendStatus(201);
-    } catch (err) {
-      console.log(err);
-      res.sendStatus(500);
+  const user = req.body;
+  const hashPassword = bcrypt.hashSync(user.password, 10);
+
+  try {
+    await db.collection("users").insertOne({ ...user, password: hashPassword });
+    res.sendStatus(201);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+});
+
+server.post("/sign-in", async (req, res) => {
+    const userInfo = req.body;
+    const token = uuid();
+
+    const user = await db.collection("users").findOne({email: userInfo.email});
+
+    if(user && bcrypt.compareSync(userInfo.password, user.password)) {
+        await db.collection("sessions").insertOne({
+            token, 
+            userId: user._id
+        });
+
+        delete user.password;
+
+        res.send({token, ...user})
+    } else {
+        res.sendStatus(401);
     }
-  });
+})
 
 server.listen(5000);
