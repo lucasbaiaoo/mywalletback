9b1a492cbb1611b86cc3f429e3c9c100047387ca
luskamusca <luskamuskammouse@gmail.com>
2022-11-21 00:00:51 -0300

    feat: create POST signup

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import { MongoClient, ObjectId } from "mongodb";
 import dotenv from "dotenv";
 import joi from "joi";
+import bcrypt from "bcrypt";
 
 dotenv.config();
 
@@ -20,3 +21,18 @@ db = mongoClient.db("myWallet");
 const server = express();
 server.use(cors());
 server.use(express.json());
+
+server.post("/sign-up", async (req, res) => {
+    const user = req.body;
+    const hashPassword = bcrypt.hashSync(user.password, 10);
+  
+    try {
+      await db.collection("users").insertOne({ ...user, password: hashPassword });
+      res.sendStatus(201);
+    } catch (err) {
+      console.log(err);
+      res.sendStatus(500);
+    }
+  });
+
+server.listen(5000);
